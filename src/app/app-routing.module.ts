e8c92e36bcf35242585aabe30ae094edd53ddb72
joelxcabana <joelcabana98@gmail.com>
2020-05-21 20:05:00 -0300

    agrege el punto 5 del tp3 de angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NovedadesComponent } from './components/novedades/novedades.component';
+import { AfiliacionComponent } from './components/afiliacion/afiliacion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home',component: HomeComponent },
+  { path: 'novedades',component: NovedadesComponent },
+  { path: 'afiliacion',component: AfiliacionComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NovedadesComponent } from './components/novedades/novedades.component';
+import { AfiliacionComponent } from './components/afiliacion/afiliacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NovedadesComponent,
+    AfiliacionComponent
   ],
   imports: [
     BrowserModule,
